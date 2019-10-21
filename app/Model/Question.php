c48c09fa53f5f70add163b2bf75e5f1578d99d7b
shakil3334413 <shakil3334413@gmail.com>
2019-10-22 00:34:58 +0600

    Model Controller migration creat

---
+++ app/Model/Question.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+class Question extends Model
+{
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function replys(){
+        return $this->hasMany(Reply:class);
+    }
+    public function catagory(){
+        return $this->belongsTo(Catagory::class);
+    }
+}
---
+++ database/factories/CatagoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Catagory;
+use Faker\Generator as Faker;
+
+$factory->define(Catagory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
